fe1b3377d7cc736024645ac20439feb3edc4ac3d
tinoji <pot5.sunen.on.the.earth.614@gmail.com>
2021-05-26 22:46:10 +0900

    Add: prevent repeated clicks

--- renderer.js
+++ renderer.js
@@ -34,53 +34,25 @@ async function show() {
 function addEventlisteners() {
     const emojis = Array.from(document.getElementsByClassName('emoji'));
     emojis.map((el) => {
-        el.addEventListener('click', async (event) => {
-            event.preventDefault();
-            displayLoading(el);
-            await sleep(0); // FIXME: hmm...
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            console.log(e.target.alt);
+
+            // prevent repeated clicks
+            let container = document.querySelector('.main-container');
+            container.style['pointer-events'] = 'none';
+            setTimeout(() => {
+                container.style['pointer-events'] = 'auto';
+            }, 1000);
 
             const v = parseInt(document.getElementById('png-size-input').value);
             if (!/^\d*$/.test(v) || v === '' || parseInt(v) === 0 || parseInt(v) > 4096) {
                 window.api.notifyError('Invalid size value.');
             } else {
-                const size = parseInt(v);
-                window.api.download(el.src, size);
-                await sleep(0);
-                window.api.notifySuccess('Copied PNG image to clipboard!');
+                window.api.download(el.src, parseInt(v));
+                window.api.notifySuccess('Copied image to clipboard!');
             }
-            hideLoading(el);
         });
     });
-}
-
-function displayLoading(el) {
-    const rect = el.getBoundingClientRect();
-    const x = rect.left + window.pageXOffset - 14;
-    const y = rect.top + window.pageYOffset - 14;
-
-    let loading = document.getElementById('loading');
-    loading.style.left = x + 'px';
-    loading.style.top = y + 'px';
-    loading.style.display = 'inline-block';
-
-    let overlay = document.getElementById('overlay');
-    overlay.style.display = 'inline-block';
-
-    el.style.zIndex = '3';
-    el.style.backgroundColor = 'rgba(233, 233, 233, 1)';
-}
-
-function hideLoading(el) {
-    let loading = document.getElementById('loading');
-    loading.style.display = 'none';
-
-    let overlay = document.getElementById('overlay');
-    overlay.style.display = 'none';
-
-    el.style.zIndex = null;
-    el.style.backgroundColor = null;
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
 }
